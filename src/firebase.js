a64d22a87ce031b15bf98d8a1f252ddab9788969
ajshan23 <ajmalshahan23@gmail.com>
2024-02-20 19:32:12 +0530

    small change in conf

--- src/firebase.js
+++ src/firebase.js
@@ -11,9 +11,14 @@ import { getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //config
+  apiKey: "AIzaSyBhzEQi1-fT5_rtpHdEuVKUpSKcTdeq7sQ",
+  authDomain: "evee-90c0c.firebaseapp.com",
+  projectId: "evee-90c0c",
+  storageBucket: "evee-90c0c.appspot.com",
+  messagingSenderId: "523870396766",
+  appId: "1:523870396766:web:34d440f7b3657dda606fcf",
+  measurementId: "G-J1MP2Z4WDT"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
